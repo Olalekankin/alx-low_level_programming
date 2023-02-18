ddc9657611c4b7e79f02aed9e111f5737d2a5b3c
olalekankin <olalekankin@gmail.com>
2023-02-17 17:57:17 -0800

    hmmmmmm

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,20 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
- * main - main block
- * Description: Get a random it with 5
- * Return 0
+ * main - print alphabet in lowercase
+ * Return: 0
  */
 int main(void)
 {
 	char c = 'a';
 
-	while (c <= 'z')
+	for (c 'a'; c <= 'z'; c++)
 	{
 		putchar(c);
-		c++;
 	}
-
 	putchar('\n');
-	return(0);
+	return (0);
 }
